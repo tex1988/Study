b590d5efb842abe73e695cf32fb94af9b84cc5ad
Tex <zinkevich-as@ukr.net>
2020-03-23 18:08:20 +0200

    Study Start!!

--- src/com/home/StringTask/StringTask2020.java
+++ src/com/home/StringTask/StringTask2020.java
@@ -1,4 +1,17 @@
 package com.home.StringTask;
 
 public class StringTask2020 {
+    public static void main(String[] args) {
+        String str = "sds;ddelfgd;lkdfg";
+        String del;
+        String newStr;
+        for (int i=0; i<str.length()-3; i++) {
+            del=str.substring(i,i+3);
+            if (del.equals("del")) {
+                newStr = str.substring(0,i)+str.substring(i+3,str.length());
+                System.out.println(newStr);
+                break;
+            }
+        }
+    }
 }
